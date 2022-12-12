44518f25a81bbe74f9d4ae4facd5d0910316f27f
edwardsuwirya <edward_suwirya@yahoo.com>
2022-12-12 16:51:58 +0700

    Add global variable example

--- day02/02a-this.js
+++ day02/02a-this.js
@@ -1,33 +1,40 @@
-// let x = 20
-//
-// const myObj = {
-//     x: 50,
-//     foo: function () {
-//         console.log(this.x)
-//     }
-// }
-//
-// myObj.foo()
-//
-// const foo = myObj.foo
-// foo()
+//if you forgot to declare a variable with var ,
+// JavaScript would merrily assume you were referring to a global variable
+// "use strict";
+x = 20
+
+const myObj = {
+    x: 50,
+    foo: function () {
+        // Registered as global
+        // abc = 10;
+        console.log(this.x)
+    }
+}
+
+myObj.foo()
+
+const foo = myObj.foo
+foo()
+console.log(global)
+
 
 //explisit
-function foo(name, address) {
-    let x = 10;
-    console.log(name, address, this.x)
-}
-
-foo()
-const anotherObj = {
-    x: 100
-}
-//Call, parameter tambahan berupa variadic
-foo.call(anotherObj, 'fadley', 'ragunan')
-
-//apply, parameter tambahan berupa array
-foo.apply(anotherObj, ['fadley', 'ragunan'])
-
-//bind, paling sering digunakan
-const fadley = foo.bind(anotherObj, 'fadley', 'ragunan')
-fadley();
+// function foo(name, address) {
+//     let x = 10;
+//     console.log(name, address, this.x)
+// }
+//
+// foo()
+// const anotherObj = {
+//     x: 100
+// }
+// //Call, parameter tambahan berupa variadic
+// foo.call(anotherObj, 'fadley', 'ragunan')
+//
+// //apply, parameter tambahan berupa array
+// foo.apply(anotherObj, ['fadley', 'ragunan'])
+//
+// //bind, paling sering digunakan
+// const fadley = foo.bind(anotherObj, 'fadley', 'ragunan')
+// fadley();
